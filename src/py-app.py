c6063466d76ce7f564bfb357c6fbfacb93c976f8
aumthakkar <aumthakkar@yahoo.co.uk>
2025-09-19 00:49:00 +0100

    changed src/** to instigate a ci run

--- src/py-app.py
+++ src/py-app.py
@@ -11,7 +11,7 @@ def info():
     return jsonify({
         "time" : time.ctime(),
         "hostname" : socket.gethostname(),
-        "message" : "Hi, Pranav! You are doing great, human!"
+        "message" : "Hi, Pranav! You are doing great, man!"
     })
 
 @app.route('/api/v1/healthz')
